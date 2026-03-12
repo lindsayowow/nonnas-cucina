d63a7a2f9d470b188bf5068bfbc19c9ef51f0110
lindsayowow <lindsayow@gmail.com>
2026-03-11 20:41:14 -0500

    loading empty css files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <div>
-      <h2>This is my footer.</h2>
+      <footer>
+
+        <Link to="/about">Contact Us</Link>
+       <p>&copy; 2026 Nonna's Cucina -- Making ordering safer for families with dietary needs</p>
+</footer>
     </div>
   )
 }
