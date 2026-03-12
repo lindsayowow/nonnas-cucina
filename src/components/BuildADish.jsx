b664ab45db94ceefa3771f50f19a33c8eb194087
lindsayowow <lindsayow@gmail.com>
2026-03-11 22:36:50 -0500

    adding button component files

--- src/components/BuildADish.jsx
+++ src/components/BuildADish.jsx
@@ -5,16 +5,18 @@ import Filters from './Filters.jsx';
 import Ingredients from './Ingredients.jsx';
 import Dish from './Dish.jsx';
 import Order from './Order.jsx';
+import IngredientButton from './IngredientButton.jsx';
+import FilterButton from './FilterButton.jsx';
 
 export default function BuildADish() {
   return (
     <div className="build-container">
       <Filters />
       <Ingredients />
-        <h2>Step 3: Verify your Selections</h2>
-        <NonnaReaction />
-        <Dish />
-        <Order />
+      <h2>Step 3: Verify your Selections</h2>
+      <NonnaReaction />
+      <Dish />
+      <Order />
     </div>
   )
 }
---
+++ src/components/FilterButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/Button.css';
+
+export default function FilterButton() {
+  return (
+    <div>
+      <p>This is a button</p>
+    </div>
+  )
+}
