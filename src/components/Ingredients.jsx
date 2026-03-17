6cd4cba95915e3e7c77534a5543589aeb11fd994
lindsayowow <lindsayow@gmail.com>
2026-03-16 22:21:37 -0500

    saving changes for end of day

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -5,15 +5,20 @@ import IngredientButton from './IngredientButton.jsx';
 export default function Ingredients(props) {
 
   return (
-          <div className="ingredientClass">
-            <h2>Choose Your Ingredients</h2>
+    <div className="ingredientClass">
+      <h2>Choose Your Ingredients</h2>
 
-            <h3>{props.Categories[0]}</h3>
-            <IngredientButton Categories={props.Categories}/>
-            <h3>{props.Categories[1]}</h3>
-            <h3>{props.Categories[2]}</h3>
-            <h3>{props.Categories[3]}</h3>
-            <h3>{props.Categories[4]}</h3>
-          </div>
+      <h3>{props.Categories[0]}</h3>
+      <IngredientButton Categories={props.Categories}
+        selectedFilter={props.selectedFilter}
+        selectedIngredient={props.selectedIngredient}
+        onSelectedIngredient={props.onSelectedIngredient}
+        selectedCategory={props.selectedCategory}
+        onSelectedCategory={props.onSelectedCategory} />
+      <h3>{props.Categories[1]}</h3>
+      <h3>{props.Categories[2]}</h3>
+      <h3>{props.Categories[3]}</h3>
+      <h3>{props.Categories[4]}</h3>
+    </div>
   )
 }
