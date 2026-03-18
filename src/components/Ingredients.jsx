47af243d6d25e638fa7cb287bffc4e0a78a1f134
lindsayowow <lindsayow@gmail.com>
2026-03-17 21:07:56 -0500

    working on ingredients component

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/BuildADish.css';
 import IngredientButton from './IngredientButton.jsx';
-import IngredientsData from '../data/ingredients.json';
+// import IngredientsData from '../data/ingredients.json';
 
 export default function Ingredients(props) {
 
@@ -9,19 +9,11 @@ export default function Ingredients(props) {
     <div className="ingredientClass">
       <h2>Choose Your Ingredients</h2>
 
-      <h3>{props.Categories[0]}</h3>
-      <IngredientButton 
-        IngredientsData={props.IngredientsData}
-        Categories={props.Categories}
-        selectedFilter={props.selectedFilter}
-        selectedIngredient={props.selectedIngredient}
-        onSelectedIngredient={props.onSelectedIngredient}
-        selectedCategory={props.selectedCategory}
-        onSelectedCategory={props.onSelectedCategory} />
-      <h3>{props.Categories[1]}</h3>
-      <h3>{props.Categories[2]}</h3>
-      <h3>{props.Categories[3]}</h3>
-      <h3>{props.Categories[4]}</h3>
+      {props.Categories.map((category) => (
+        <h3 key={category}>{category}</h3>
+      ))}
+
+      <IngredientButton  />
     </div>
   )
 }
