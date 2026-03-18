ffbc4c3710c6de0dc29a7b57177610ca8a44b24f
lindsayowow <lindsayow@gmail.com>
2026-03-17 21:48:47 -0500

    versioning on category logic map

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import '../styles/BuildADish.css';
 import IngredientButton from './IngredientButton.jsx';
-// import IngredientsData from '../data/ingredients.json';
+import IngredientsData from '../data/ingredients.json';
+import categoryMap from '../utils/constants.js'
 
 export default function Ingredients(props) {
 
   return (
     <div className="ingredientClass">
       <h2>Choose Your Ingredients</h2>
+      {props.Categories.map((Category) => (
+        <div className="categoryContainer">
+          <h3 key={Category} className="categoryHeader">{Category}</h3>
 
-      {props.Categories.map((category) => (
-        <h3 key={category}>{category}</h3>
+          {IngredientsData.filter((ingredient) =>
+              ingredient.category === Category)
+              .map((ingredient) => (<IngredientButton key={ingredient.name}
+                name={ingredient.name} />
+              ))}
+
+        </div>
       ))}
 
-      <IngredientButton  />
     </div>
-  )
+  );
 }
