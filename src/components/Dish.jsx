c54d614922ab15eca039be66e72f74e75e844904
lindsayowow <lindsayow@gmail.com>
2026-03-22 21:25:55 -0500

    working on dish component

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import '../styles/BuildADish.css';
 
-export default function Dish(ingredient) {
+export default function Dish({ selectedIngredients }) {
     return (
-        <div className="dish">
-            <h3>Your Dish</h3>
-            <p>Add to Order button - moves list to Your Order as 1 array with ingredient objects</p>
-            <ul>
-                selectedIngredients.map((emoji, name)
-                    <li>{ingredient.emoji} {ingredient.name}</li>
-                )
-            </ul>
+        <div key="dish" className="dish">
+            <h2>Your Dish</h2>
+            <button>Add to Order button - give this functionality</button>
+
+            {selectedIngredients.length === 0 ? (
+                <div className="empty">
+                    <p className="dishEmoji">🍽️</p>
+                    <p>Your dish is empty.</p></div>
+            ) : (
+                <ul>
+                    {selectedIngredients.map(ingredient => (
+                        <li key={ingredient.name}>
+                            {ingredient.emoji} {ingredient.name} - {new Intl.NumberFormat("en-US", {
+                                style: "currency",
+                                currency: "USD"
+                            }).format(ingredient.price)}
+                        </li>
+                    ))}
+
+                    <p>Order total: </p>
+                </ul>
+            )}
         </div>
-    )
+    );
 }
