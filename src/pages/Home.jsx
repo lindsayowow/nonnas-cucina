ecd240860cef56ef6da56198ddacaf82bee795f5
lindsayowow <lindsayow@gmail.com>
2026-03-30 23:53:49 -0500

    finishing home styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,21 +7,24 @@ import FourSquare from '../components/FourSquare';
 export default function Home() {
   return (
     <div className="hero">
-
-      <p className="mangia left top">Mangia</p>
-      <p className="mangia left bottom">Mangia</p>
-
-      <div className="nonna-wrapper">
-        <img
-          src={NonnaHome}
-          alt="Nonna is smiling at a countertop with hearts surrounding her head"
-          className="logononna"
-        />
+      <div className="hero-row">
+        <div className="hero-column hero-left">
+          <p className="mangia left top">Mangia</p>
+          <p className="mangia left bottom">Mangia</p>
+        </div>
+        <div className="hero-column hero-center">
+          <div className="nonna-wrapper">
+            <img
+              src={NonnaHome}
+              alt="Nonna is smiling at a countertop with hearts surrounding her head"
+              className="logononna" />
+          </div>
+        </div>
+        <div className="hero-column hero-right">
+          <p className="mangia right top">Mangia</p>
+          <p className="mangia right bottom">Mangia</p>
+        </div>
       </div>
-
-      <p className="mangia right top">Mangia</p>
-      <p className="mangia right bottom">Mangia</p>
-
       <h1 className="nonna-font">Benvenuti a Nonna's Cucina!</h1>
 
       <h2>Build safe, delicious dishes that meet you and your family's dietary needs</h2>
