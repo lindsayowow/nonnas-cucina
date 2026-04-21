c86d031bb658d7f6270c274dda63893206a2414d
lindsayowow <lindsayow@gmail.com>
2026-04-20 23:11:47 -0500

    filter accessibility updates

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,8 +5,14 @@ import ClearFilterButton from './buttons/ClearFilterButton.jsx';
 
 export default function Filters(props) {
   return (
-    <div className="card filterClass">
-      <h2 className="text-center">Dietary Filters</h2>
+    // region landmark for screen readers
+    <div
+      className="card filterClass"
+      role="region"
+      aria-labelledby="filters-title"
+    >
+      <h2 id="filters-title" className="text-center">Dietary Filters</h2>
+
       {/* Parent passing props to child */}
       <FilterButton
         DietaryFilters={props.DietaryFilters}
